6db636a54849c140c2b5dfccbefd3a03ce390771
Sonia D <davson1987@gmail.com>
2019-04-27 22:55:16 -0700

    Updates

---
+++ precache-manifest.3e6ec87f3d1f2bd01d9febd62572ddb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdecd0678b9d69a44afc",
+    "url": "/clicky_game/static/css/main.04c9b9ab.chunk.css"
+  },
+  {
+    "revision": "bdecd0678b9d69a44afc",
+    "url": "/clicky_game/static/js/main.4f2fa3e7.chunk.js"
+  },
+  {
+    "revision": "ef6ad64b0df3315f3b6a",
+    "url": "/clicky_game/static/js/runtime~main.33627756.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky_game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "990e0652a28b5fc2faae6a114451aa63",
+    "url": "/clicky_game/index.html"
+  }
+];
